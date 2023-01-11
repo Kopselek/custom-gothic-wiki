cf1178b7818a40d12a18f7b72e3b63d1ea6440a9
Filip Łapiński <lapinski.filipp@gmail.com>
2023-01-11 19:42:18 +0100

    xml config

--- Utils/XMLConfigBase.php
+++ Utils/XMLConfigBase.php
@@ -7,6 +7,11 @@ abstract class XMLConfigBase
     public function __construct(
         private string $configName
     ) {
-        $xml = new SimpleXMLElement($this->configName);
+        $xml = new SimpleXMLElement("<" . $this->configName . "></" . $this->configName . ">");
+
+        $xml->addAtributa("test", "test");
+        Header('Content-type: text/xml');
+
+        echo $xml->asXML();
     }
 }
