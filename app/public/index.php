736d9d190352e555c5a7de97dc79b171e384972a
Filip Łapiński <lapinski.filipp@gmail.com>
2023-01-16 21:01:43 +0100

    fix form

--- app/public/index.php
+++ app/public/index.php
@@ -19,16 +19,16 @@
 <body>
 
 <form action="./Ajax/creatures.ajax.php" name="creaturesForm" method="POST">
-    <label for="index">Index</label>
+    <label for="index[]">Index</label>
     <input type="text" name="index">
     <label for="level">Level</label>
     <input type="number" name="level">
     <h2>---------</h2>
     <h2>Name</h2>
     <label for="name-polish">Polish</label>
-    <input type="text" name="name-polish">
+    <input type="text" name="name[polish]">
     <label for="name-english">English</label>
-    <input type="text" name="name-english">
+    <input type="text" name="name[english]">
     <h2>---------</h2>
     <label for="instance">Instance</label>
     <input type="text" name="instance">
@@ -51,13 +51,13 @@
     <h2>---------</h2>
     <h2>Damage</h2>
     <label for="meele-damage">Meele</label>
-    <input type="number" name="meele-damage">
+    <input type="number" name="damage[meele]">
     <label for="meeleweapon-damage">Meele weapon</label>
-    <input type="number" name="meeleweapon-damage">
+    <input type="number" name="damage[meeleweapon]">
     <label for="ranged-damage">Ranged weapon</label>
-    <input type="number" name="ranged-damage">
+    <input type="number" name="damage[ranged]">
     <label for="magic">Magic</label>
-    <input type="number" name="magic">
+    <input type="number" name="damage[magic]">
     <h2>---------</h2>
 
     <div id="drop">
@@ -67,61 +67,61 @@
         <div class="drop-1">
             <h3>Item 1</h3>
             <label for="drop-instance">Instance</label>
-            <input type="text" name="drop-instance">
+            <input type="text" name="drop[1][instance]">
             <label for="drop-min">Min</label>
-            <input type="number" name="drop-min">
+            <input type="number" name="drop[1][min]">
             <label for="drop-max">Max</label>
-            <input type="number" name="drop-max">
+            <input type="number" name="drop[1][max]">
             <label for="drop-chance">Chance</label>
-            <input type="number" name="drop-chance">
+            <input type="number" name="drop[1][chance]">
         </div>
 
         <div class="drop-2" hidden>
             <h3>Item 2</h3>
             <label for="drop-instance">Instance</label>
-            <input type="text" name="drop-instance">
+            <input type="text" name="drop[2][instance]">
             <label for="drop-min">Min</label>
-            <input type="number" name="drop-min">
+            <input type="number" name="drop[2][min]">
             <label for="drop-max">Max</label>
-            <input type="number" name="drop-max">
+            <input type="number" name="drop[2][max]">
             <label for="drop-chance">Chance</label>
-            <input type="number" name="drop-chance">
+            <input type="number" name="drop[2][chance]">
         </div>
 
         <div class="drop-3" hidden>
             <h3>Item 3</h3>
             <label for="drop-instance">Instance</label>
-            <input type="text" name="drop-instance">
+            <input type="text" name="drop[3][instance]">
             <label for="drop-min">Min</label>
-            <input type="number" name="drop-min">
+            <input type="number" name="drop[3][min]">
             <label for="drop-max">Max</label>
-            <input type="number" name="drop-max">
+            <input type="number" name="drop[3][max]">
             <label for="drop-chance">Chance</label>
-            <input type="number" name="drop-chance">
+            <input type="number" name="drop[3][chance]">
         </div>
 
         <div class="drop-4" hidden>
             <h3>Item 4</h3>
             <label for="drop-instance">Instance</label>
-            <input type="text" name="drop-instance">
+            <input type="text" name="drop[4][instance]">
             <label for="drop-min">Min</label>
-            <input type="number" name="drop-min">
+            <input type="number" name="drop[4][min]">
             <label for="drop-max">Max</label>
-            <input type="number" name="drop-max">
+            <input type="number" name="drop[4][max]">
             <label for="drop-chance">Chance</label>
-            <input type="number" name="drop-chance">
+            <input type="number" name="drop[4][chance]">
         </div>
 
         <div class="drop-5" hidden>
             <h3>Item 5</h3>
             <label for="drop-instance">Instance</label>
-            <input type="text" name="drop-instance">
+            <input type="text" name="drop[5][instance]">
             <label for="drop-min">Min</label>
-            <input type="number" name="drop-min">
+            <input type="number" name="drop[5][min]">
             <label for="drop-max">Max</label>
-            <input type="number" name="drop-max">
+            <input type="number" name="drop[5][max]">
             <label for="drop-chance">Chance</label>
-            <input type="number" name="drop-chance">
+            <input type="number" name="drop[5][chance]">
         </div>
 
 
@@ -129,15 +129,15 @@
     <h2>---------</h2>
     <h2>Protection</h2>
     <label for="protection-edge">Edge</label>
-    <input type="text" name="protection-edge">
+    <input type="text" name="protection[edge]">
     <label for="protection-blunt">Blunt</label>
-    <input type="number" name="protection-blunt">
+    <input type="number" name="protection[blunt]">
     <label for="protection-point">Point</label>
-    <input type="number" name="protection-point">
+    <input type="number" name="protection[point]">
     <label for="protection-fire">Fire</label>
-    <input type="number" name="protection-fire">
+    <input type="number" name="protection[fire]">
     <label for="protection-magic">Magic</label>
-    <input type="number" name="protection-magic">
+    <input type="number" name="protection[magic]">
     <h2>---------</h2>
     <label for="mindistance">Min distance</label>
     <input type="number" name="mindistance">
@@ -150,41 +150,27 @@
     <h2>---------</h2>
     <h2>Weapon</h2>
     <label for="weapon-meeleweapon">Meele weapon</label>
-    <input type="number" name="weapon-meeleweapon">
+    <input type="number" name="weapon[meeleweapon]">
     <label for="weapon-armor">Armor</label>
-    <input type="number" name="weapon-armor">
+    <input type="number" name="weapon[armor]">
     <label for="weapon-shield">Shield</label>
-    <input type="number" name="weapon-shield">
+    <input type="number" name="weapon[shield]">
     <label for="weapon-magic">Magic</label>
-    <input type="number" name="weapon-magic">
+    <input type="number" name="weapon[magic]">
     <h2>---------</h2>
     <h2>Visual</h2>
     <label for="visual-bodymodel">Body model</label>
-    <input type="number" name="visual-bodymodel">
+    <input type="number" name="visual[bodymodel]">
     <label for="visual-bodytexture">Body texture</label>
-    <input type="number" name="visual-bodytexture">
+    <input type="number" name="visual[bodytexture]">
     <label for="visual-headmodel">Head model</label>
-    <input type="number" name="visual-headmodel">
+    <input type="number" name="visual[headmodel]">
     <label for="visual-headtexture">Head texture</label>
-    <input type="number" name="visual-headtexture">
+    <input type="number" name="visual[headtexture]">
     <h2>---------</h2>
     <button type="submit">Create</button>
 
 </form>
-<script>
-    function handleSubmit(event) {
-        event.preventDefault();
-
-        const data = new FormData(event.target);
-
-        const value = Object.fromEntries(data.entries());
-
-        console.log({value});
-    }
-
-    const form = document.querySelector('#form');
-    form.addEventListener('submit', handleSubmit);
-</script>
 </body>
 
 </html>
