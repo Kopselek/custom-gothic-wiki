526ae012eb89e80b1ad4df6d6ecd07c9829724d8
Filip Łapiński <lapinski.filipp@gmail.com>
2023-01-25 13:18:43 +0100

    filling manager and config

--- app/public/ajax/items.ajax.php
+++ app/public/ajax/items.ajax.php
@@ -42,6 +42,43 @@ $namePl = $_POST['name_pl'];
 $nameEng = $_POST['name_eng'];
 
 $model = new ItemsConfig();
+$model->setMode($mode);
+$model->setInstance($instance);
+$model->setTexture($texture);
+$model->setLevel($level);
+$model->setPrice($price);
+$model->setCrystalSlots($crystalSlots);
+$model->setStregth($strength);
+$model->setDexterity($dexterity);
+$model->setInteligence($inteligence);
+$model->setEquipLevel($equipLevel);
+
+$model->setDamageIncisors($damageIncisors);
+$model->setDamageHammer($damageHammer);
+$model->setDamageStab($damageStab);
+$model->setDamageElement($damageElement);
+$model->setDamageMagic($damageMagic);
+
+$model->setEffectMode($effectMode);
+$model->setEffectType($effectType);
+$model->setEffectValue($effectValue);
+$model->setEffectTime($effectTime);
+
+$model->setBonusStrengthMode($bonusStrengthMode);
+$model->setBonusStrengthValue($bonusStrengthValue);
+
+$model->setBonusDexterityMode($bonusDexterityMode);
+$model->setBonusDexterityValue($bonusDexterityValue);
+
+$model->setBonusInteligenceMode($bonusInteligenceMode);
+$model->setBonusInteligenceValue($bonusInteligenceValue);
+
+$model->setDamageMonsterBonusMode($damageMonsterBonusMode);
+$model->setDamageMonsterBonusType($damageMonsterBonusType);
+$model->setDamageMonsterBonusValue($damageMonsterBonusValue);
+
+$model->setNamePl($namePl);
+$model->setNameEng($nameEng);
 
 $itemsManager = new ItemsManager();
 $itemsManager->create($model);
--- app/src/Manager/ItemsManager.php
+++ app/src/Manager/ItemsManager.php
@@ -15,6 +15,36 @@ class ItemsManager extends BaseManager
 
         $stmt->execute([
             'type' => $model->getType(),
+            'mode' => $model->getMode(),
+            'instance' => $model->getInstance(),
+            'texture' => $model->getTexture(),
+            'level' => $model->getLevel(),
+            'price' => $model->getPrice(),
+            'crystalslots' => $model->getCrystalSlots(),
+            'strength' => $model->getStregth(),
+            'dexterity' => $model->getDexterity(),
+            'inteligence' => $model->getInteligence(),
+            'equiplevel' => $model->getEquipLevel(),
+            'damage_incisors' => $model->getDamageIncisors(),
+            'damage_hammer' => $model->getDamageHammer(),
+            'damage_stab' => $model->getDamageStab(),
+            'damage_element' => $model->getDamageElement(),
+            'damage_magic' => $model->getDamageMagic(),
+            'effect_mode' => $model->getEffectMode(),
+            'effect_type' => $model->getEffectType(),
+            'effect_value' => $model->getEffectValue(),
+            'effect_time' => $model->getEffectTime(),
+            'bonusstrength_mode' => $model->getBonusStrengthMode(),
+            'bonusstrength_value' => $model->getBonusStrengthValue(),
+            'bonusdexterity_mode' => $model->getBonusDexterityMode(),
+            'bonusdexterity_value' => $model->getBonusDexterityValue(),
+            'bonusinteligence_mode' => $model->getBonusInteligenceMode(),
+            'bonusinteligence_value' => $model->getBonusInteligenceValue(),
+            'damagemonsterbonus_mode' => $model->getDamageMonsterBonusMode(),
+            'damagemonsterbonus_value' => $model->getDamageMonsterBonusValue(),
+            'damagemonsterbonus_type' => $model->getDamageMonsterBonusType(),
+            'name_pl' => $model->getNamePl(),
+            'name_eng' => $model->getNameEng()
         ]);
     }
 }
